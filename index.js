b273a84afca3a274bcf592b58759c9aa0dbfecd5
DialecticalLaw <d.shmuratkin@mail.ru>
2023-11-05 20:53:34 +0300

    feat: add animation to the code section

---
+++ index.js
@@ -0,0 +1,95 @@
+const fallingSyntax = document.querySelector('.falling-syntax');
+
+const jsSyntax = ['return', 'if', 'else', 'switch', 'for', 'while', 'break', 'case', 'this', 'typeof', 'addEventListener', 'querySelector', 'document', 'function', 'undefined', 'null', 'NaN', 'true', 'false', 'let', 'const', 'i++', 'i--', '||', '&amp;&amp;'];
+
+setInterval(() => {
+    showDepartingElements()
+}, 550);
+
+function showDepartingElements() {
+    const randomSyntax = Math.floor(Math.random() * 25);
+    const randomLeft = Math.floor(Math.random() * 101);
+
+    if (randomSyntax <= 9) {
+        const newElem = document.createElement('code');
+        newElem.classList.add('js-syntax');
+        newElem.innerHTML = jsSyntax[randomSyntax];
+        newElem.setAttribute('style', `left: ${randomLeft}%; display: block; color: rgb(254, 148, 254);`);
+        fallingSyntax.insertAdjacentElement('afterbegin', newElem);
+
+        setTimeout(() => {
+            newElem.style.transform = 'translateY(550%)';
+            newElem.style.opacity = '0';
+            setTimeout(() => {
+                newElem.remove();
+            }, 3000);
+        }, 30);
+    } else if (randomSyntax >= 10 && randomSyntax <= 11) {
+        const newElem = document.createElement('code');
+        newElem.classList.add('js-syntax');
+        newElem.innerHTML = jsSyntax[randomSyntax];
+        newElem.setAttribute('style', `left: ${randomLeft}%; display: block; color: rgb(211, 211, 104);`);
+        fallingSyntax.insertAdjacentElement('afterbegin', newElem);
+
+        setTimeout(() => {
+            newElem.style.transform = 'translateY(550%)';
+            newElem.style.opacity = '0';
+            setTimeout(() => {
+                newElem.remove();
+            }, 3000);
+        }, 30);
+        
+    } else if (randomSyntax >= 12 && randomSyntax <= 20) {
+        const newElem = document.createElement('code');
+        newElem.classList.add('js-syntax');
+        newElem.innerHTML = jsSyntax[randomSyntax];
+        newElem.setAttribute('style', `left: ${randomLeft}%; display: block; color: rgb(125, 159, 255);`);
+        fallingSyntax.insertAdjacentElement('afterbegin', newElem);
+
+        setTimeout(() => {
+            newElem.style.transform = 'translateY(550%)';
+            newElem.style.opacity = '0';
+            setTimeout(() => {
+                newElem.remove();
+            }, 3000);
+        }, 30);
+    } else {
+        const newElem = document.createElement('code');
+        newElem.classList.add('js-syntax');
+        newElem.innerHTML = jsSyntax[randomSyntax];
+        newElem.setAttribute('style', `left: ${randomLeft}%; display: block;`);
+        fallingSyntax.insertAdjacentElement('afterbegin', newElem);
+
+        setTimeout(() => {
+            newElem.style.transform = 'translateY(550%)';
+            newElem.style.opacity = '0';
+            setTimeout(() => {
+                newElem.remove();
+            }, 3000);
+        }, 30);
+    }
+}
+
+
+
+// const navContacts = document.querySelector('[href="#contacts"]');
+// const navSummary = document.querySelector('[href="#summary"]');
+// const navSkills = document.querySelector('[href="#skills"]');
+// const navEducation = document.querySelector('[href="#education"]');
+// const navLanguages = document.querySelector('[href="#languages"]');
+// const navProjects = document.querySelector('[href="#projects"]');
+// const navCode = document.querySelector('[href="#code"]');
+
+
+// document.addEventListener('click', highlightSection);
+
+// function highlightSection(event) {
+//     switch (event.target) {
+//         case value:
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+// }
